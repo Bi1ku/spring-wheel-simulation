e22aa799d4a629c54ac82c2307000e96e271a0ca
schen479 <schen9358@gmail.com>
2026-05-22 13:43:17 -0400

    setting up file and venv

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def main():
-    print("Hello, World!")
+from vpython import *
 
+AXIS = (0, 0, 0)
+
+class Wheel: 
+    def __init__(self, radius):
+        cylinder(pos = vec(0, 0, 0), axis = vec(0, 0, 1), radius = radius, color = color.red)
 
 if __name__ == "__main__":
-    main()
+    wheel = Wheel(2)
