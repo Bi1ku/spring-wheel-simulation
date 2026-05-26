b7d602f8cd16c82e3b4e8ee3ff6ad4fddaf1c72a
Bi1ku <2008owenshi@gmail.com>
2026-05-26 13:35:41 -0400

    add in preliminary spring code

--- main.py
+++ main.py
@@ -1,6 +1,56 @@
 from vpython import *
 
-class Simulation: 
+ROD_X = -screen.width + 10
+NUM_SPRINGS = 1
+
+
+def bind_num_springs(evt):
+    print(evt.value)
+    NUM_SPRINGS = evt.value
+
+
+def bind_len_springs(evt):
+    if evt.id == "1":
+        pass
+    elif evt.id == "2":
+        pass
+    elif evit.id == "3":
+        pass
+
+
+class Spring:
+    def __init__(self, length, y_pos, spring_constant):
+        self.spring_length = length
+        self.spring_position = vector(ROD_X, y_pos, 0)
+        self.spring_constant = spring_constant
+
+    def draw(self):
+        helix(
+            pos=self.spring_position,
+            axis=vec(1, 0, 0),
+            color=color.cyan,
+            radius=100,
+            length=self.spring_length,
+        )
+
+
+def menu():
+    slider(bind=bind_num_springs, max=3, min=1, step=1, value=NUM_SPRINGS)
+
+
+def setup():
+    scene.background = color.white
+    menu()
+    curve(
+        pos=[vec(ROD_X, -screen.height, 0), vector(ROD_X, screen.height, 0)],
+        color=color.black,
+        radius=10,
+    )
+    spring = Spring(100, 6, 10)
+    spring.draw()
+
+
+class Simulation:
     def __init__(self, wheelR, axelR):
         self.wheel = Wheel(wheelR, 1.0, [vec(0, 0.25, 0), vec(0, -0.25, 0)])
         self.axis = Axis(axelR)
@@ -10,41 +60,56 @@ class Simulation:
     def loop(self):
         self.wheel.display()
 
-class Wheel: 
+
+class Wheel:
     def __init__(self, radius, mass, springLocations):
-        self.radius = radius 
+        self.radius = radius
         self.length = 1
         self.springLocations = springLocations
         self.calculateMomentOfInertia()
 
     def calculateMomentOfInertia(self):
-        self.mInertia = (0.5 * self.mass * math.pow(self.radius, 2))
+        self.mInertia = 0.5 * self.mass * math.pow(self.radius, 2)
 
     def applyTorque(self, force, lArm):
         pass
-    
+
     def changeMass(self, mass):
-        self.mass = mass 
+        self.mass = mass
         calculateMomentOfInertia()
 
     def changeRadius(self, radius):
-        self.radius = radius 
+        self.radius = radius
         calculateMomentOfInertia()
-        
-    def display(self):
-        self.cylinder = cylinder(pos = vec(0, 0, 0), axis = vec(0, 0, 1), radius = self.radius, length = self.length, color = color.red)
-        self.springPoints = points(pos = self.springLocations, color = color.gray)
 
-        #self.cylinder.rotate(axis = vec(0,0,0), angle = pi/3)
+    def display(self):
+        self.cylinder = cylinder(
+            pos=vec(0, 0, 0),
+            axis=vec(0, 0, 1),
+            radius=self.radius,
+            length=self.length,
+            color=color.red,
+        )
+        self.springPoints = points(pos=self.springLocations, color=color.gray)
+
+        # self.cylinder.rotate(axis = vec(0,0,0), angle = pi/3)
+
+
 class Axis:
     def __init__(self, radius):
-        self.radius = radius 
+        self.radius = radius
         self.length = 1.5
-    
+
     def display(self):
-        cylinder(pos = vec(0, 0, 0), axis = vec(0, 0, 1), radius = self.radius, length = self.length, color = color.yellow)
+        cylinder(
+            pos=vec(0, 0, 0),
+            axis=vec(0, 0, 1),
+            radius=self.radius,
+            length=self.length,
+            color=color.yellow,
+        )
+
 
 if __name__ == "__main__":
     simulation = Simulation(0.5, 0.1)
     simulation.loop()
-    
